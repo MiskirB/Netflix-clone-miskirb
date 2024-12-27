e66292e00b8aac9349e807d3c90c0d6c28a2ba6e
MiskirB <156695703+MiskirB@users.noreply.github.com>
2024-12-27 23:40:08 +0300

    Update Header.js
    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Header.css";
-import netflixLogo from "../../assets/images/netflixlogo.png";
+import netflixLogo from "../assets/images/netflixlogo.png";
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
